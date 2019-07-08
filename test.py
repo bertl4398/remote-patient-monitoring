d16f11cc953ef1dfa8486143ec1dc58d2cc29a63
Benjamin Ertl <benjamin.ertl@asamnet.de>
2019-07-08 10:13:11 +0200

    update

---
+++ test.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
+out = cv.VideoWriter('output.mp4', fourcc, 20.0, (640,  480))
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame = cv.flip(frame, 1)
+    out.write(frame)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+out.release()
+cv.destroyAllWindows()
